1c9d81d693d85f5e0842688144ba3aefcb9a2590
Apriadi <apriadim>
2019-12-01 17:15:05 +0700

    add todo mongo

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,120 +1,73 @@
-import React from 'react';
-import { withRouter } from "react-router-dom";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { withRouter, Link } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import axios from "axios";
 import { Formik, ErrorMessage } from "formik";
 import { loginValidation } from "../validate";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://apriadi11blog.wordpress.com/">
-        apriadi11blog.wordpress.com
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
 }
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    "@global": {
+        body: {
+            backgroundColor: theme.palette.common.white
+        }
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1)
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2)
+    }
 }));
 
 function SignIn(props) {
-  // constructor(props) {
-  //     super(props)
+    const classes = useStyles();
 
-  //     this.state = {
-  //          email:"",
-  //          password:""
-  //     }
-  // }
-  // this.setState()
+    const API = process.env.REACT_APP_API_LIVE;
 
-  const classes = useStyles();
-
-  // const [signIn, setSignIn] = React.useState({ email: "", password: "" });
-
-  // const handleSubmit = event => {
-  //     event.preventDefault();
-
-  //     const user = JSON.parse(localStorage.getItem("user"));
-
-  //     if (user.email === signIn.email && user.password === signIn.password) {
-  //         localStorage.setItem("isLogin", JSON.stringify(true));
-
-  //         if (JSON.parse(localStorage.getItem("isLogin")) === true) {
-  //             props.history.push("/");
-  //         }
-  //     } else {
-  //         alert("Email atau Password salah");
-  //     }
-  // };
-
-  // const handleChange = event => {
-  //     // this.setState({
-  //     //     ...this.state,
-  //     //     [event.target.name]: event.target.value
-  //     // })
-  //     setSignIn({
-  //         ...signIn,
-  //         [event.target.name]: event.target.value
-  //     });
-  // };
-
-  const API = process.env.REACT_APP_API_LIVE;
-  
-  return (
-    <Container component="main" className={classes.root}>
-      <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          {/* <form className={classes.form} noValidate 
-                    onSubmit={handleSubmit}> */}
-            
-            <Formik
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Formik
                     initialValues={{
                         email: "",
                         password: ""
